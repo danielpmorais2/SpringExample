d80533e7c3b0ddbea1c5ec883e0af61b1ae9ceb3
Daniel Pereira de Morais <pet0163601@aluno.firjansenaisesi.com.br>
2026-04-13 16:37:55 -0300

    repository

--- controle_frequencia/src/main/java/com/senai/backend/controle_frequencia/repositories/PautaRepository.java
+++ controle_frequencia/src/main/java/com/senai/backend/controle_frequencia/repositories/PautaRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.senai.backend.controle_frequencia.models.Pauta;
-import com.senai.backend.controle_frequencia.models.Usuario;
 @Repository
 public interface PautaRepository extends JpaRepository <Pauta, Integer> {
 
